9f50091f14f156cdc195788ce1be2e9cf69a0a9b
Anurag choudhury <idanurag567@gmail.com>
2023-10-07 16:09:45 +0530

    changed io version 2.0.0 +fixed a minor bug

--- public/scripts/helper.js
+++ public/scripts/helper.js
@@ -1,2 +1,2 @@
-// export const base_url = "https://passwizard-vault.up.railway.app";
-export const base_url = "http://127.0.0.1:3001";
+export const base_url = "https://passwizard-vault.up.railway.app";
+// export const base_url = "http://127.0.0.1:3001";
